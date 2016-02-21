f5ed06f952f8cce4c4482be226835ceb009429b5
Mickaël Bergem <mickael@securem.eu>
2016-02-21 18:42:18 +0100

    Add gotBottom JS script

---
+++ _includes/JB/bottom.js
@@ -0,0 +1,3 @@
+<script>var gotBottom=false; window.onscroll = function(ev) {
+    if (gotBottom===false && (window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100) {
+       gotBottom = true; _paq.push(['trackEvent', 'ReadEvent', 'ReadEvent.reachedBottom', document.title]);}};</script>
