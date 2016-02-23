f67ad2fe8b5070a201168c7211e49eda51cf56d6
Mickaël Bergem <mickael@securem.eu>
2016-02-23 15:28:11 +0100

    Improve the reachedBottom tracking script

--- _includes/JB/bottom.js
+++ _includes/JB/bottom.js
@@ -1,9 +1,6 @@
 <script>
-var _paq = _paq || [];
-window.addEventListener("load", function load(event){
+var _paq = _paq || []; window.addEventListener("load", function load(event){
     _paq.push(['trackEvent', 'ReadEvent', 'ReadEvent.startedReading', document.title]);
-});
-var gotBottom=false;
-window.addEventListener("scroll", function(ev) {
-    if (gotBottom===false && (window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100) {
+}); var gotBottom=false; window.addEventListener("scroll", function(ev) {
+    if (gotBottom===false && (window.innerHeight + window.scrollY) >= document.body.offsetHeight - 1000) {
        gotBottom = true; _paq.push(['trackEvent', 'ReadEvent', 'ReadEvent.reachedBottom', document.title]);}});</script>
